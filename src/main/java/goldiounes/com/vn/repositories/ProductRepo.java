5b9f82854c534c0778425ea37aef5233291d47b1
Nguyễn Lê Hiếu Nhi <nhinhi7ahs@gmail.com>
2024-07-27 10:55:33 +0700

    Add controller, service, repo

--- src/main/java/goldiounes/com/vn/repositories/ProductRepo.java
+++ src/main/java/goldiounes/com/vn/repositories/ProductRepo.java
@@ -1,16 +1,14 @@
 package goldiounes.com.vn.repositories;
 
-import goldiounes.com.vn.models.Category;
+import goldiounes.com.vn.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepo extends JpaRepository<Category, Integer> {
+public interface ProductRepo extends JpaRepository<Product, Integer> {
 
-    @Query("select c from Category c where c.CategoryName=:CategoryName")
-    Category findByName(@Param("CategoryName") String CategoryName);
-
-
+    @Query("select p from Product p where p.ProductName=:ProductName")
+    public Product findByName(@Param("ProductName") String ProductName);
 }
--- src/main/java/goldiounes/com/vn/services/CategoryService.java
+++ src/main/java/goldiounes/com/vn/services/CategoryService.java
@@ -1,11 +1,32 @@
 package goldiounes.com.vn.services;
 
+import goldiounes.com.vn.models.Category;
 import goldiounes.com.vn.repositories.CategoryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
     @Autowired
     private CategoryRepo categoryRepo;
+    public List<Category> findAll() {
+        return categoryRepo.findAll();
+    }
+    public Category findById(int id) {
+        return categoryRepo.findById(id).get();
+    }
+    public Category save(Category category) {
+        return categoryRepo.save(category);
+    }
+    public void delete(Category category) {
+        categoryRepo.delete(category);
+    }
+    public void deleteById(int id) {
+        categoryRepo.deleteById(id);
+    }
+    public Category findByName(String name) {
+        return categoryRepo.findByName(name);
+    }
 }
