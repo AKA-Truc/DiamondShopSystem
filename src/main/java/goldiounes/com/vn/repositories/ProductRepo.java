9596a3513a90c2a8a26637cd9f824c1f2563a96c
AKA-Truc <pthanhtruca3@gmail.com>
2024-07-27 12:25:28 +0700

    Update ProductRepo.java

--- src/main/java/goldiounes/com/vn/repositories/ProductRepo.java
+++ src/main/java/goldiounes/com/vn/repositories/ProductRepo.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Integer> {
 
     @Query("select p from Product p where p.ProductName=:ProductName")
-    public Product findByName(@Param("ProductName") String ProductName);
+    List<Product> findByProductName(@Param("ProductName") String ProductName);
 }
