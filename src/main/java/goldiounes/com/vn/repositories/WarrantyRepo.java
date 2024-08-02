3684ae0d8d1ff6c923b45e8719c0f1bbaa5704f1
Huy Hoang <163020090+DemonQuang@users.noreply.github.com>
2024-08-02 23:56:27 +0700

    add controllers and receiptTest

--- src/main/java/goldiounes/com/vn/repositories/WarrantyRepo.java
+++ src/main/java/goldiounes/com/vn/repositories/WarrantyRepo.java
@@ -3,9 +3,15 @@ package goldiounes.com.vn.repositories;
 
 import goldiounes.com.vn.models.Warranty;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface WarrantyRepo extends JpaRepository<Warranty, Integer> {
 
+    @Query ("select u from User u where u.UserID=:userid")
+    public List<Warranty> findByUserID(@Param("userid") int userid);
 }
---
+++ src/test/java/goldiounes/com/vn/models/ReceiptTest.java
@@ -0,0 +1,46 @@
+package goldiounes.com.vn.models;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ReceiptTest {
+    @Test
+    void testGetterAndSetter() {
+        Product product = new Product();
+        Receipt receipt = new Receipt(product,10);
+
+        assertEquals(10, receipt.getQuantity());
+        assertEquals(product, receipt.getProduct());
+
+        Category category = new Category("AAA");
+        Product product1 = new Product(category,"BBB","URL",
+                1.2,500,600,24,10);
+        receipt.setProduct(product1);
+        receipt.setQuantity(100);
+
+        assertEquals(100, receipt.getQuantity());
+        assertEquals(product1, receipt.getProduct());
+    }
+
+    @Test
+    void testDefaultConstructor() {
+        Receipt receipt = new Receipt();
+
+        assertNotNull(receipt);
+        assertNull(receipt.getProduct());
+        assertEquals(0,receipt.getQuantity());
+    }
+
+    @Test
+    void testConstructor(){
+        Category category = new Category("AAA");
+        Product product = new Product(category,"BBB","URL",
+                1.2,500,600,24,10);
+        Receipt receipt = new Receipt(product,10);
+
+        assertNotNull(receipt);
+        assertNotNull(receipt.getProduct());
+        assertEquals(10,receipt.getQuantity());
+    }
+}
