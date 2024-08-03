0de19fa8d3825f2d3054b35e50da4f7486689946
Haimee17204 <huynhmy10715@gmail.com>
2024-08-03 12:43:23 +0700

    fix DiamondDetailTest

--- src/test/java/goldiounes/com/vn/models/DiamondDetailTest.java
+++ src/test/java/goldiounes/com/vn/models/DiamondDetailTest.java
@@ -1,46 +1,50 @@
 package goldiounes.com.vn.models;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-import org.mockito.Mockito;
 
-class DiamondDetailTest {
+public class DiamondDetailTest {
 
     @Test
-    void testDiamondDetailGettersAndSetters() {
+    void testGetterAndSetters() {
+        Diamond diamond = new Diamond();
+        ProductDetail productDetail = new ProductDetail();
+        int diamondDetailID = 1;
+        int quantity = 10;
+
         DiamondDetail diamondDetail = new DiamondDetail();
+        diamondDetail.setDiamondDetailID(diamondDetailID);
+        diamondDetail.setDiamond(diamond);
+        diamondDetail.setProductDetail(productDetail);
+        diamondDetail.setQuantity(quantity);
 
-        Diamond mockDiamond = Mockito.mock(Diamond.class);
-        ProductDetail mockProductDetail = Mockito.mock(ProductDetail.class);
-
-        diamondDetail.setDiamondDetailID(1);
-        diamondDetail.setDiamond(mockDiamond);
-        diamondDetail.setProductDetail(mockProductDetail);
-        diamondDetail.setQuantity(5);
-
-        assertEquals(1, diamondDetail.getDiamondDetailID());
-        assertEquals(mockDiamond, diamondDetail.getDiamond());
-        assertEquals(mockProductDetail, diamondDetail.getProductDetail());
-        assertEquals(5, diamondDetail.getQuantity());
+        assertEquals(diamondDetailID, diamondDetail.getDiamondDetailID());
+        assertEquals(diamond, diamondDetail.getDiamond());
+        assertEquals(quantity, diamondDetail.getQuantity());
+        assertEquals(productDetail, diamondDetail.getProductDetail());
     }
 
     @Test
-    void testDiamondDetailConstructor() {
-        Diamond mockDiamond = Mockito.mock(Diamond.class);
+    void testConstructor() {
+        Diamond diamond = new Diamond();
+        int quantity = 5;
 
-        DiamondDetail diamondDetail = new DiamondDetail(mockDiamond, 5);
+        DiamondDetail diamondDetail = new DiamondDetail(diamond, quantity);
 
-        assertEquals(mockDiamond, diamondDetail.getDiamond());
-        assertEquals(5, diamondDetail.getQuantity());
+        assertNotNull(diamondDetail);
+        assertEquals(diamond, diamondDetail.getDiamond());
+        assertEquals(quantity, diamondDetail.getQuantity());
     }
 
     @Test
-    void testDiamondDetailProductDetailRelationship() {
+    void testDefaultConstructor() {
         DiamondDetail diamondDetail = new DiamondDetail();
+        assertNotNull(diamondDetail);
 
-        ProductDetail mockProductDetail = Mockito.mock(ProductDetail.class);
-        diamondDetail.setProductDetail(mockProductDetail);
-
-        assertEquals(mockProductDetail, diamondDetail.getProductDetail());
+        assertEquals(0, diamondDetail.getDiamondDetailID());
+        assertNull(diamondDetail.getDiamond());
+        assertEquals(0, diamondDetail.getQuantity());
+        assertNull(diamondDetail.getProductDetail());
     }
 }
