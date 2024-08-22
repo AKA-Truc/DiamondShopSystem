dc49c143eaf78f101f8a0e06eb8fb7ff0011867c
AKA-Truc <pthanhtruca3@gmail.com>
2024-08-22 15:38:10 +0700

    cmt all file Token

--- src/main/java/goldiounes/com/vn/config/SecurityConfig.java
+++ src/main/java/goldiounes/com/vn/config/SecurityConfig.java
@@ -1,68 +1,68 @@
-package goldiounes.com.vn.config;
-
-import goldiounes.com.vn.models.entities.User;
-import goldiounes.com.vn.repositories.UserRepo;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration
-public class SecurityConfig {
-
-    private final UserRepo userRepo;
-    Logger logger = LogManager.getLogger(SecurityConfig.class);
-    @Autowired
-    public SecurityConfig(UserRepo userRepo) {
-        this.userRepo = userRepo;
-        logger.info("UserRepository bean initialized");
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return email -> {
-            User user = userRepo.findByEmail(email);
-            if (user == null) {
-                throw new RuntimeException("Cannot find user with email =" + email);
-            }
-            return convertToUserDetails(user);
-        };
-    }
-
-    private UserDetails convertToUserDetails(User user) {
-        return org.springframework.security.core.userdetails.User.builder()
-                .username(user.getUserName())
-                .password(user.getPassword())
-                .roles(user.getRole())
-                .build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService());
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
-    }
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration config
-    ) throws Exception {
-        return config.getAuthenticationManager();
-    }
-
-}
+//package goldiounes.com.vn.config;
+//
+//import goldiounes.com.vn.models.entities.User;
+//import goldiounes.com.vn.repositories.UserRepo;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@Configuration
+//public class SecurityConfig {
+//
+//    private final UserRepo userRepo;
+//    Logger logger = LogManager.getLogger(SecurityConfig.class);
+//    @Autowired
+//    public SecurityConfig(UserRepo userRepo) {
+//        this.userRepo = userRepo;
+//        logger.info("UserRepository bean initialized");
+//    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        return email -> {
+//            User user = userRepo.findByEmail(email);
+//            if (user == null) {
+//                throw new RuntimeException("Cannot find user with email =" + email);
+//            }
+//            return convertToUserDetails(user);
+//        };
+//    }
+//
+//    private UserDetails convertToUserDetails(User user) {
+//        return org.springframework.security.core.userdetails.User.builder()
+//                .username(user.getUserName())
+//                .password(user.getPassword())
+//                .roles(user.getRole())
+//                .build();
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public AuthenticationProvider authenticationProvider(){
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        authProvider.setUserDetailsService(userDetailsService());
+//        authProvider.setPasswordEncoder(passwordEncoder());
+//        return authProvider;
+//    }
+//    @Bean
+//    public AuthenticationManager authenticationManager(
+//            AuthenticationConfiguration config
+//    ) throws Exception {
+//        return config.getAuthenticationManager();
+//    }
+//
+//}
--- src/main/java/goldiounes/com/vn/services/TokenService.java
+++ src/main/java/goldiounes/com/vn/services/TokenService.java
@@ -1,91 +1,91 @@
-package goldiounes.com.vn.services;
-
-import goldiounes.com.vn.components.JwtTokenUtils;
-import goldiounes.com.vn.models.entities.Token;
-import goldiounes.com.vn.models.entities.User;
-import goldiounes.com.vn.repositories.TokenRepository;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.UUID;
-
-@Service
-@RequiredArgsConstructor
-public class TokenService {
-    private static final int MAX_TOKENS = 3;
-    @Value("${jwt.expiration}")
-    private int expiration; //save to an environment variable
-
-    @Value("${jwt.expiration-refresh-token}")
-    private int expirationRefreshToken;
-
-    private final TokenRepository tokenRepository;
-    private final JwtTokenUtils jwtTokenUtil;
-
-
-    @Transactional
-    public Token addToken(User user, String token, boolean isMobileDevice) {
-        List<Token> userTokens = tokenRepository.findByUser(user);
-        int tokenCount = userTokens.size();
-        if (tokenCount >= MAX_TOKENS) {
-            boolean hasNonMobileToken = !userTokens.stream().allMatch(Token::isMobile);
-            Token tokenToDelete;
-            if (hasNonMobileToken) {
-                tokenToDelete = userTokens.stream()
-                        .filter(userToken -> !userToken.isMobile())
-                        .findFirst()
-                        .orElse(userTokens.get(0));
-            } else {
-                tokenToDelete = userTokens.get(0);
-            }
-            tokenRepository.delete(tokenToDelete);
-        }
-        long expirationInSeconds = expiration;
-        LocalDateTime expirationDateTime = LocalDateTime.now().plusSeconds(expirationInSeconds);
-        Token newToken = Token.builder()
-                .user(user)
-                .token(token)
-                .revoked(false)
-                .expired(false)
-                .tokenType("Bearer")
-                .expirationDate(expirationDateTime)
-                .isMobile(isMobileDevice)
-                .build();
-        newToken.setRefreshToken(UUID.randomUUID().toString());
-        newToken.setRefreshExpirationDate(LocalDateTime.now().plusSeconds(expirationRefreshToken));
-        tokenRepository.save(newToken);
-        return newToken;
-
-    }
-
-    public Token refreshToken(String refreshToken, User user) throws Exception {
-        Token existingToken = tokenRepository.findByRefreshToken(refreshToken);
-        if (existingToken == null) {
-            throw new Exception("Token not found");
-        }
-        if(existingToken.getRefreshExpirationDate().compareTo(LocalDateTime.now()) < 0){
-            tokenRepository.delete(existingToken);
-            throw new Exception("Refresh token is expired");
-        }
-        String token = jwtTokenUtil.generateToken(user);
-        LocalDateTime expirationDateTime = LocalDateTime.now().plusSeconds(expiration);
-        existingToken.setExpirationDate(expirationDateTime);
-        existingToken.setToken(token);
-        existingToken.setRefreshToken(UUID.randomUUID().toString());
-        existingToken.setRefreshExpirationDate(LocalDateTime.now().plusSeconds(expirationRefreshToken));
-        return existingToken;
-    }
-
-    public void deleteToken(String token) {
-        Token tokenEntity = tokenRepository.findByToken(token);
-        if (tokenEntity != null) {
-            tokenRepository.delete(tokenEntity);
-        } else {
-            throw new RuntimeException("Token not found.");
-        }
-    }
-}
+//package goldiounes.com.vn.services;
+//
+//import goldiounes.com.vn.components.JwtTokenUtils;
+//import goldiounes.com.vn.models.entities.Token;
+//import goldiounes.com.vn.models.entities.User;
+//import goldiounes.com.vn.repositories.TokenRepository;
+//import jakarta.transaction.Transactional;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//
+//import java.time.LocalDateTime;
+//import java.util.List;
+//import java.util.UUID;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class TokenService {
+//    private static final int MAX_TOKENS = 3;
+//    @Value("${jwt.expiration}")
+//    private int expiration; //save to an environment variable
+//
+//    @Value("${jwt.expiration-refresh-token}")
+//    private int expirationRefreshToken;
+//
+//    private final TokenRepository tokenRepository;
+//    private final JwtTokenUtils jwtTokenUtil;
+//
+//
+//    @Transactional
+//    public Token addToken(User user, String token, boolean isMobileDevice) {
+//        List<Token> userTokens = tokenRepository.findByUser(user);
+//        int tokenCount = userTokens.size();
+//        if (tokenCount >= MAX_TOKENS) {
+//            boolean hasNonMobileToken = !userTokens.stream().allMatch(Token::isMobile);
+//            Token tokenToDelete;
+//            if (hasNonMobileToken) {
+//                tokenToDelete = userTokens.stream()
+//                        .filter(userToken -> !userToken.isMobile())
+//                        .findFirst()
+//                        .orElse(userTokens.get(0));
+//            } else {
+//                tokenToDelete = userTokens.get(0);
+//            }
+//            tokenRepository.delete(tokenToDelete);
+//        }
+//        long expirationInSeconds = expiration;
+//        LocalDateTime expirationDateTime = LocalDateTime.now().plusSeconds(expirationInSeconds);
+//        Token newToken = Token.builder()
+//                .user(user)
+//                .token(token)
+//                .revoked(false)
+//                .expired(false)
+//                .tokenType("Bearer")
+//                .expirationDate(expirationDateTime)
+//                .isMobile(isMobileDevice)
+//                .build();
+//        newToken.setRefreshToken(UUID.randomUUID().toString());
+//        newToken.setRefreshExpirationDate(LocalDateTime.now().plusSeconds(expirationRefreshToken));
+//        tokenRepository.save(newToken);
+//        return newToken;
+//
+//    }
+//
+//    public Token refreshToken(String refreshToken, User user) throws Exception {
+//        Token existingToken = tokenRepository.findByRefreshToken(refreshToken);
+//        if (existingToken == null) {
+//            throw new Exception("Token not found");
+//        }
+//        if(existingToken.getRefreshExpirationDate().compareTo(LocalDateTime.now()) < 0){
+//            tokenRepository.delete(existingToken);
+//            throw new Exception("Refresh token is expired");
+//        }
+//        String token = jwtTokenUtil.generateToken(user);
+//        LocalDateTime expirationDateTime = LocalDateTime.now().plusSeconds(expiration);
+//        existingToken.setExpirationDate(expirationDateTime);
+//        existingToken.setToken(token);
+//        existingToken.setRefreshToken(UUID.randomUUID().toString());
+//        existingToken.setRefreshExpirationDate(LocalDateTime.now().plusSeconds(expirationRefreshToken));
+//        return existingToken;
+//    }
+//
+//    public void deleteToken(String token) {
+//        Token tokenEntity = tokenRepository.findByToken(token);
+//        if (tokenEntity != null) {
+//            tokenRepository.delete(tokenEntity);
+//        } else {
+//            throw new RuntimeException("Token not found.");
+//        }
+//    }
+//}
