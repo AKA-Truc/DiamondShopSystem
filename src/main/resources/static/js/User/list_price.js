e4b90b405526e59cf55acb4ddf836f189fd1b664
Haimee17204 <huynhmy10715@gmail.com>
2024-08-29 23:47:46 +0700

    add fontend

--- src/main/resources/static/js/User/list_price.js
+++ src/main/resources/static/js/User/list_price.js
@@ -8,11 +8,11 @@ function rotateText() {
         centerText.textContent = textOptions[textIndex];
         centerText.classList.add('visible');
         textIndex = (textIndex + 1) % textOptions.length;
-    }, 1000); // Wait for the fade-out effect before changing text
+    }, 1000);
 }
 
-setInterval(rotateText, 2000); // Change text every 3 seconds (1s fade-out + 1s fade-in)
-rotateText(); // Initialize text
+setInterval(rotateText, 2000);
+rotateText();
 
 // icon click
 document.addEventListener("DOMContentLoaded", function () {
@@ -116,13 +116,89 @@ const diamondData = {
 
     '5 Ly 4': [
         {ly: '5.4', color: 'D', sach: 'IF', price: '125,680,000', discount: '115,625,000'},
+        {ly: '5.4', color: 'D', sach: 'VVS1', price: '125,680,000', discount: '108,790,000'},
+        {ly: '5.4', color: 'D', sach: 'VVS2', price: '91,990,000', discount: '84,630,000'},
+        {ly: '5.4', color: 'D', sach: 'VS1', price: '81,468,000', discount: '74,950,000'},
+        {ly: '5.4', color: 'D', sach: 'VS2', price: '71,620,000', discount: '65,890,000'},
+        {ly: '5.4', color: 'E', sach: 'IF', price: '99,990,000', discount: '91,990,000'},
+        {ly: '5.4', color: 'E', sach: 'VVS1', price: '94,990,000', discount: '87,390,000'},
+        {ly: '5.4', color: 'E', sach: 'VVS2', price: '88,468,000', discount: '81,390,000'},
+        {ly: '5.4', color: 'E', sach: 'VS1', price: '73,685,000', discount: '67,790,000'},
+        {ly: '5.4', color: 'E', sach: 'VS2', price: '66,250,000', discount: '60,950,000'},
+        {ly: '5.4', color: 'F', sach: 'IF', price: '93,250,000', discount: '85,790,000'},
+        {ly: '5.4', color: 'F', sach: 'VVS1', price: '84,620,000', discount: '77,850,000'},
+        {ly: '5.4', color: 'F', sach: 'VVS2', price: '69,539,000', discount: '63,975,000'},
+        {ly: '5.4', color: 'F', sach: 'VS1', price: '65,745,000', discount: '60,485,000'},
+        {ly: '5.4', color: 'F', sach: 'VS2', price: '61,685,000', discount: '56,750,000'},
+        {ly: '5.4', color: 'G', sach: 'VS1', price: '56,979,000', discount: '52,420,000'},
+        {ly: '5.4', color: 'G', sach: 'VS2', price: '52,500,000', discount: '48,300,000'},
     ],
-    '6 Ly 8': [
-        { id: 'D005', color: 'Xám', price: '$700' },
-        { id: 'D006', color: 'Nâu', price: '$750' },
+    '6 Ly': [
+        { ly: '6.0', color: 'D', sach: 'IF', price: '256,468,000', discount: '235,950,000' },
+        { ly: '6.0', color: 'D', sach: 'VVS1', price: '237,500,000', discount: '218,500,000' },
+        { ly: '6.0', color: 'D', sach: 'VVS2', price: '194,990,000', discount: '179,390,000' },
+        { ly: '6.0', color: 'D', sach: 'VS1', price: '172,479,000', discount: '158,680,000' },
+        { ly: '6.0', color: 'D', sach: 'VS2', price: '153,250,000', discount: '140,990,000' },
+        { ly: '6.0', color: 'E', sach: 'IF', price: '213,979,000', discount: '196,860,000' },
+        { ly: '6.0', color: 'E', sach: 'VVS1', price: '199,990,000', discount: '183,990,000' },
+        { ly: '6.0', color: 'E', sach: 'VVS2', price: '183,468,000', discount: '168,790,000' },
+        { ly: '6.0', color: 'E', sach: 'VS1', price: '158,468,000', discount: '145,790,000' },
+        { ly: '6.0', color: 'E', sach: 'VS2', price: '148,805,000', discount: '136,900,000' },
+        { ly: '6.0', color: 'F', sach: 'IF', price: '202,990,000', discount: '186,750,000' },
+        { ly: '6.0', color: 'F', sach: 'VVS1', price: '186,805,000', discount: '171,860,000' },
+        { ly: '6.0', color: 'F', sach: 'VVS2', price: '177,990,000', discount: '163,750,000' },
     ],
 
-    // Add more data here as needed
+    '6 Ly 3': [
+        {ly: '6.3', color: 'D', sach: 'IF', price: '412,990,000', discount: '379,950,000'},
+        {ly: '6.3', color: 'D', sach: 'VVS1', price: '357,990,000', discount: '329,350,000'},
+        {ly: '6.3', color: 'D', sach: 'VVS2', price: '285,750,000', discount: '262,890,000'},
+        {ly: '6.3', color: 'D', sach: 'VS1', price: '256,979,000', discount: '236,420,000'},
+        {ly: '6.3', color: 'D', sach: 'VS2', price: '247,805,000', discount: '227,980,000'},
+        {ly: '6.3', color: 'E', sach: 'IF', price: '346,468,000', discount: '318,750,000'},
+        {ly: '6.3', color: 'E', sach: 'VVS1', price: '323,250,000', discount: '297,390,000'},
+        {ly: '6.3', color: 'E', sach: 'VVS2', price: '271,685,000', discount: '249,950,000'},
+        {ly: '6.3', color: 'E', sach: 'VS1', price: '247,805,000', discount: '227,980,000'},
+        {ly: '6.3', color: 'E', sach: 'VS2', price: '228,805,000', discount: '210,500,000'},
+        {ly: '6.3', color: 'F', sach: 'IF', price: '325,935,000', discount: '299,860,000'},
+        {ly: '6.3', color: 'F', sach: 'VVS1', price: '303,805,000', discount: '279,500,000'},
+        {ly: '6.3', color: 'F', sach: 'VVS2', price: '260,750,000', discount: '239,890,000'},
+        {ly: '6.3', color: 'F', sach: 'VS1', price: '237,990,000', discount: '218,950,000'},
+        {ly: '6.3', color: 'F', sach: 'VS2', price: '214,990,000', discount: '197,790,000'},
+        {ly: '6.3 (>=1CT)', color: 'D', sach: 'IF', price: '542,120,000', discount: '498,750,000'},
+        {ly: '6.3 (>=1CT)', color: 'D', sach: 'VVS1', price: '455,979,000', discount: '419,500,000'},
+        {ly: '6.3 (>=1CT)', color: 'D', sach: 'VVS2', price: '383,468,000', discount: '352,790,000'},
+        {ly: '6.3 (>=1CT)', color: 'D', sach: 'VS1', price: '325,745,000', discount: '299,685,000'},
+        {ly: '6.3 (>=1CT)', color: 'D', sach: 'VS2', price: '308,468,000', discount: '283,790,000'},
+        {ly: '6.3 (>=1CT)', color: 'E', sach: 'IF', price: '464,990,000', discount: '427,790,000'},
+        {ly: '6.3 (>=1CT)', color: 'E', sach: 'VVS1', price: '378,805,000', discount: '348,500,000'},
+        {ly: '6.3 (>=1CT)', color: 'E', sach: 'VVS2', price: '368,968,000', discount: '339,450,000'},
+        {ly: '6.3 (>=1CT)', color: 'E', sach: 'VS1', price: '311,685,000', discount: '286,750,000'},
+        {ly: '6.3 (>=1CT)', color: 'E', sach: 'VS2', price: '299,990,000', discount: '275,990,000'},
+        {ly: '6.3 (>=1CT)', color: 'F', sach: 'IF', price: '389,990,000', discount: '358,790,000'},
+        {ly: '6.3 (>=1CT)', color: 'F', sach: 'VVS1', price: '368,995,000', discount: '339,475,000'},
+        {ly: '6.3 (>=1CT)', color: 'F', sach: 'VVS2', price: '347,479,000', discount: '319,680,000'},
+        {ly: '6.3 (>=1CT)', color: 'F', sach: 'VS1', price: '285,750,000', discount: '262,890,000'},
+        {ly: '6.3 (>=1CT)', color: 'F', sach: 'VS2', price: '279,620,000', discount: '257,250,000'},
+    ],
+
+    '6 Ly 8':[
+        {ly: '6.8', color: 'D', sach: 'IF', price: '683,685,000', discount: '628,990,000'},
+        {ly: '6.8', color: 'D', sach: 'VVS1', price: '598,685,000', discount: '550,790,000'},
+        {ly: '6.8', color: 'D', sach: 'VVS2', price: '489,990,000', discount: '450,790,000'},
+        {ly: '6.8', color: 'D', sach: 'VS1', price: '448,685,000', discount: '412,790,000'},
+        {ly: '6.8', color: 'D', sach: 'VS2', price: '433,468,000', discount: '398,790,000'},
+        {ly: '6.8', color: 'E', sach: 'IF', price: '541,250,000', discount: '497,950,000'},
+        {ly: '6.8', color: 'E', sach: 'VVS1', price: '539,990,000', discount: '496,790,000'},
+        {ly: '6.8', color: 'E', sach: 'VVS2', price: '432,500,000', discount: '397,900,000'},
+        {ly: '6.8', color: 'E', sach: 'VS1', price: '404,979,000', discount: '372,580,000'},
+        {ly: '6.8', color: 'E', sach: 'VS2', price: '346,468,000', discount: '318,750,000'},
+        {ly: '6.8', color: 'F', sach: 'IF', price: '498,685,000', discount: '458,790,000'},
+        {ly: '6.8', color: 'F', sach: 'VVS1', price: '488,979,000', discount: '449,860,000'},
+        {ly: '6.8', color: 'F', sach: 'VVS2', price: '368,250,000', discount: '338,790,000'},
+        {ly: '6.8', color: 'F', sach: 'VS1', price: '383,468,000', discount: '352,790,000'},
+        {ly: '6.8', color: 'F', sach: 'VS2', price: '346,468,000', discount: '318,750,000'},
+    ],
 };
 
 function changeData(diamondType) {
@@ -143,6 +219,12 @@ function changeData(diamondType) {
         introText.style.display = 'block';
     } else if (diamondType === '7 Ly 2') {
         introText.innerHTML = 'Kim cương 7 Ly 2 là lựa chọn hoàn hảo cho .....';
+        introText.style.display = 'block';
+    } else if (diamondType === '8 Ly') {
+        introText.innerHTML = 'Kim cương 8 Ly là lựa chọn hoàn hảo cho .....';
+        introText.style.display = 'block';
+    } else if (diamondType === '8 Ly 1') {
+        introText.innerHTML = 'Kim cương 8 Ly 1 là lựa chọn hoàn hảo cho .....';
         introText.style.display = 'block';
     } else{
         introText.style.display = 'none';
