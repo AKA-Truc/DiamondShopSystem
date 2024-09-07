3cde7ae8f6ebb62bf41fa3e5cf61ebbe24018f3c
Nguyễn Lê Hiếu Nhi <nhinhi7ahs@gmail.com>
2024-09-07 13:58:29 +0700

    fix admin

--- src/main/resources/static/js/url.js
+++ src/main/resources/static/js/url.js
@@ -1,1 +1,17 @@
 window.base_url = "http://localhost:8080";
+
+function logout() {
+    fetch(`${window.base_url}/user-management/logout`,{
+        method: 'DELETE',
+        headers:{
+            'Authorization': `Bearer ${localStorage.getItem('authToken')}`,
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(data =>{
+            localStorage.removeItem('authToken');
+            window.location.href = '/DiamondShopSystem/src/main/resources/templates/login.html';
+        })
+        .catch(error=>{
+            console.log(error);})
+}
