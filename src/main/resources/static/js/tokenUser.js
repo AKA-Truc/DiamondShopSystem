68df7a8ae762ef9e72f686f21109b6c78c1495c9
AKA-Truc <pthanhtruca3@gmail.com>
2024-09-12 11:57:58 +0700

    check token

---
+++ src/main/resources/static/js/tokenUser.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function (){
+    const token = localStorage.getItem('authToken');
+    const tokenPayload = JSON.parse(atob(token.split('.')[1]));
+    const roles = tokenPayload.roles;
+    const role = roles[0];
+    console.log(role);
+
+    if (!token) {
+        window.location.href = '/DiamondShopSystem/src/main/resources/templates/User/about_us.html';
+    }
+})
